9ff2e4d77ac41bee05a78e1102ee8d605fcac0d3
Jon Camuso <work.jcamuso@proton.me>
2023-10-10 10:51:04 -0500

    Add test remove console.logs

--- ticker-web-api/index.js
+++ ticker-web-api/index.js
@@ -17,11 +17,10 @@ io.on('connection', client => {
 
     client.on('request-stocks', async (symbols) => {
         const initialLastTrades = await stockTicker.getInitialTickerData(symbols);
-        // console.log('initialLastTrades ', initialLastTrades);
         client.emit('stocks', initialLastTrades);
 
         const interval = setInterval(async () => {
-                const stocks = await stockTicker.getLastTrade(symbols);
+            const stocks = await stockTicker.getLastTrade(symbols);
             client.emit('stocks', stocks);
             console.log('emitting stocks ', stocks);
         }, 1000);
--- ticker-web-api/ports/simdaqApi.test.js
+++ ticker-web-api/ports/simdaqApi.test.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const {
     getLastTrade,
     getSymbolDetails,
-    getMultipleSymbolDetails
+    getMultipleSymbolDetails,
+    getInitialTickerData,
 } = require('../ports/simdaqApi');
 
 const fetch = require('node-fetch');
@@ -22,7 +23,7 @@ describe('simdaqApi', () => {
     });    
     describe('getLastTrade', () => {
         it('should call simdaq last trade rest endpoint with symbol parameters', async () => {
-            const symbols = 'FOO,BAZ';
+            const symbols = ['FOO','BAZ'];
             const expectedUrlHostPath = `${SIMDAQ_API_HOST}${SIMDAQ_API_LAST_TRADE_PATH}${symbols}`;
             const fakeResponse = Promise.resolve({
                 ok: true,
@@ -56,7 +57,8 @@ describe('simdaqApi', () => {
 
             expect(fetch).toHaveBeenCalledWith(expectedUrlHostPath);
         });
-
+    });
+    describe('getMultipleSymbolDetails', () => {
         it('should call simdaq reference symbol rest endpoint for all symbol parameters', async () => {
             const symbols = ['FOO','BAZ'];
             const expectedUrlHostPath = `${SIMDAQ_API_HOST}${SIMDAQ_API_SYMBOL_DETAILS_PATH}`;
@@ -76,5 +78,27 @@ describe('simdaqApi', () => {
             expect(fetch).toHaveBeenCalledWith(`${expectedUrlHostPath}${symbols[1]}`);
         });        
     });
+    describe('getInitialTickerData', () => {
+        it('should call simdaq last trade and reference symbol rest endpoints for all symbol parameters', async () => {
+            const symbols = ['FOO','BAZ'];
 
+            const expectedLastTradeUrl = `${SIMDAQ_API_HOST}${SIMDAQ_API_LAST_TRADE_PATH}${symbols}`;
+            const expectedReferenceSymbolHostPath = `${SIMDAQ_API_HOST}${SIMDAQ_API_SYMBOL_DETAILS_PATH}`;
+            const fakeResponse = Promise.resolve({
+                ok: true,
+                status: 200,
+                json: async () => {
+                    return {};
+                },
+            });
+            fetch.mockImplementation(()=> fakeResponse);
+
+            const restult = await getInitialTickerData(symbols)
+
+            expect(fetch).toHaveBeenCalledWith(expectedLastTradeUrl);
+            expect(fetch).toHaveBeenCalledWith(`${expectedReferenceSymbolHostPath}${symbols[0]}`);
+            expect(fetch).toHaveBeenCalledWith(`${expectedReferenceSymbolHostPath}${symbols[1]}`);
+        });
+
+    });
 });
