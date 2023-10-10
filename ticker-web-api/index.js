0233b3337c1da49390b3251f47d17ee6ae08ab69
Jon Camuso <work.jcamuso@proton.me>
2023-10-10 14:10:23 -0500

    add running image and remove console.log

--- ticker-web-api/index.js
+++ ticker-web-api/index.js
@@ -1,6 +1,5 @@
 // (B) Node server to handle API requests
 require('dotenv').config();
-console.log(process.env) // remove this after you've confirmed it is working
 const io = require('socket.io')(3000, {
     cors: {
         origin: [
